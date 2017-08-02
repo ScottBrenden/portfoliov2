6eaf1bc578a27fc0be7a491783ffde0f5eb12c29
ScottBrenden <scott.b.brenden@gmail.com>
2017-08-02 16:43:39 -0700

    webpack and initial package json

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Home extends React.Component{
+  render(){
+    return(
+      <h1> Scott Brenden</h1>
+    )
+  }
+}
+
+ReactDOM.render(<Home />, document.getElementById('root'))
