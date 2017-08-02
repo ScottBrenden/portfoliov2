dab550af82cc21781a67441413954aef74444499
ScottBrenden <scott.b.brenden@gmail.com>
2017-08-02 14:19:36 -0700

    server start?

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const pg = require('pg');
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('.'));
+
+app.get('*', function(request, response){
+  response.sendFile('/index.html', {root: '.'});
+});
+
+app.listen(PORT, function(){
+  console.log(`on PORT localhost:${PORT}`);
+});
