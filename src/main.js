b620e979c73b8b56532ce57362e256d912c90d1c
ScottBrenden <scott.b.brenden@gmail.com>
2017-08-08 12:43:37 -0700

    added some metas

--- src/main.js
+++ src/main.js
@@ -1,12 +1,53 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './styles/main.scss'
 
-class Home extends React.Component{
+class Navbar extends React.Component{
+  constructor(props){
+      super(props)
+      this.state = {
+        count: 0
+      }
+
+      this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(){
+    console.log('did a click')
+    this.setState(prevState => {count: prevState.count++})
+  }
+
   render(){
     return(
-      <h1> Scott Brenden</h1>
+      <div>
+        <p onClick={this.handleClick}> about </p>
+        <p onClick={this.handleClick}> home </p>
+        <p onClick={this.handleClick}> projects </p>
+        <p>{this.state.count}</p>
+      </div>
     )
   }
 }
+
+class Home extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+
+    }
+  }
+
+
+
+  render(){
+    return(
+      <div>
+        <Navbar className="home-nav" />
+        <h1> Scott Brenden</h1>
+      </div>
+    )
+  }
+}
+
 
 ReactDOM.render(<Home />, document.getElementById('root'))
