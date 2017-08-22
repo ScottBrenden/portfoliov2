d3c4550d4658967819378dd542aaa6488bdc5682
ScottBrenden <scott.b.brenden@gmail.com>
2017-08-21 19:37:44 -0700

    about page, mobile first

--- src/component/about-container/index.js
+++ src/component/about-container/index.js
@@ -6,10 +6,10 @@ export class AboutContainer extends React.Component {
   render(){
     return (
       <div className='about-container'>
-        <div className='about-pic'>
+        <div className='about-pic cols'>
           <p>pic here</p>
         </div>
-        <div className='about-text'>
+        <div className='about-text cols'>
           <h2 className='pg-title'> ABOUT </h2>
           <p className='pg-emph'> some stuff about me and who i am and what i do and why i do it </p>
           <p className='pg-content'> other stuff about me that is less important or just extra information </p>
