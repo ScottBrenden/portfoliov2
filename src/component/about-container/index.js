487551f18dcfc576d4469feb8681d035c2b24212
ScottBrenden <scott.b.brenden@gmail.com>
2017-08-23 15:56:20 -0700

    added about content and some margins work

--- src/component/about-container/index.js
+++ src/component/about-container/index.js
@@ -11,8 +11,10 @@ export class AboutContainer extends React.Component {
         </div>
         <div className='about-text cols'>
           <h2 className='pg-title'> ABOUT </h2>
-          <p className='pg-emph'> some stuff about me and who i am and what i do and why i do it </p>
-          <p className='pg-content'> other stuff about me that is less important or just extra information </p>
+          <p className='pg-emph'> My name is Scott Brenden. I am a full-stack JavaScript web developer. </p>
+          <p className='pg-content'> Growing up I became a huge baseball fan (Go Mariners!). Later on in life I relized this was largely due to my pasion for statistics and data in general. After years of working in a veriaty of constomer service rolls I noticed how much room for improvement there is how a lot data is compiled or presented. This was a significant factor in my decision to make the career change to becoming a developer.</p>
+          <p className='pg-content'> My passions as a developer are building well designed data bases, and displaying them in attractive and intuitive ways to users. Creating meaningful and well designed data bases that are intuitive to use is incredably important. Even well constructed data however can be made meaningless if poorly displayed to a user. </p>
+          <h2 className='sub-section'>Follow me at these places to learn more:</h2>
           <div id="media-links">
             <div id="linked" className="media-link">
               <a href="https://www.linkedin.com/in/scott-brenden/" className="icon-linkedin"></a>
