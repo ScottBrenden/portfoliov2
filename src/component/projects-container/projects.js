64791a323fda02c966708729862678b8e77d409b
ScottBrenden <scott.b.brenden@gmail.com>
2017-08-22 18:11:02 -0700

    added to projects index.js and scss

--- src/component/projects-container/projects.js
+++ src/component/projects-container/projects.js
@@ -1,1 +1,42 @@
+import React from 'react'
+
 import './_projects.scss'
+
+export class ProjectsContainer extends React.Component {
+  render(){
+    return(
+      <div className='projects-container'>
+        <div className='project'>
+          <div className='project-pic cols'>
+
+          </div>
+          <div className='project-info-odd cols'>
+            <h1 className='pg-title'>Looking to Game</h1>
+            <p className='pg-emph'></p>
+            <p className='pg-content'></p>
+          </div>
+        </div>
+        <div className='project'>
+          <div className='project-pic cols'>
+
+          </div>
+          <div className='project-info-even cols'>
+            <h1 className='pg-title'>Decarbonate</h1>
+            <p className='pg-emph'></p>
+            <p className='pg-content'></p>
+          </div>
+        </div>
+        <div className='project'>
+          <div className='project-pic cols'>
+
+          </div>
+          <div className='project-info-odd cols'>
+            <h1 className='pg-title'>Meteorite Mapper</h1>
+            <p className='pg-emph'></p>
+            <p className='pg-content'></p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
