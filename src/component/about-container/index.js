4150fc6b2a551fbce0928df7275935c22fe7782e
ScottBrenden <scott.b.brenden@gmail.com>
2017-08-23 16:46:14 -0700

    media link animations

--- src/component/about-container/index.js
+++ src/component/about-container/index.js
@@ -16,17 +16,21 @@ export class AboutContainer extends React.Component {
           <p className='pg-content'> My passions as a developer are building well designed data bases, and displaying them in attractive and intuitive ways to users. Creating meaningful and well designed data bases that are intuitive to use is incredably important. Even well constructed data however can be made meaningless if poorly displayed to a user. </p>
           <h2 className='sub-section'>Follow me at these places to learn more:</h2>
           <div id="media-links">
-            <div id="linked" className="media-link">
-              <a href="https://www.linkedin.com/in/scott-brenden/" className="icon-linkedin"></a>
+            <div id="linked" className="media-link card">
+              <a href="https://www.linkedin.com/in/scott-brenden/" className="icon-linkedin flip-front"></a>
+              <p className='flip-back'>Linked In</p>
             </div>
-            <div id="facebook" className="media-link">
-              <a href="https://www.facebook.com/scott.brenden.1" className="icon-facebook2"></a>
+            <div id="facebook" className="media-link card">
+              <a href="https://www.facebook.com/scott.brenden.1" className="icon-facebook2 flip-front"></a>
+              <p className='flip-back'>Facebook</p>
             </div>
-            <div id="git" className="media-link">
-              <a href="https://github.com/ScottBrenden" className="icon-github"></a>
+            <div id="git" className="media-link card">
+              <a href="https://github.com/ScottBrenden" className="icon-github flip-front"></a>
+              <p className='flip-back'>Github</p>
             </div>
-            <div id="twitter" className="media-link">
-              <a href="https://twitter.com/ScottBrenden" className="icon-twitter"></a>
+            <div id="twitter" className="media-link card">
+              <a href="https://twitter.com/ScottBrenden" className="icon-twitter flip-front"></a>
+              <p className='flip-back'>Twitter</p>
             </div>
           </div>
         </div>
