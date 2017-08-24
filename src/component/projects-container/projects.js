a2674c8c675cb0740947707e1c2bc998f0934554
ScottBrenden <scott.b.brenden@gmail.com>
2017-08-24 14:39:00 -0700

    project page styling plus content for looking to game

--- src/component/projects-container/projects.js
+++ src/component/projects-container/projects.js
@@ -8,32 +8,45 @@ export class ProjectsContainer extends React.Component {
       <div className='projects-container'>
         <div className='project'>
           <div className='project-pic cols'>
-
+            <div className='project-title-box'>
+              <h1 className='pg-title project-title'>Looking to Game</h1>
+            </div>
           </div>
           <div className='project-info-odd cols'>
-            <h1 className='pg-title'>Looking to Game</h1>
-            <p className='pg-emph'></p>
-            <p className='pg-content'></p>
+            <p className='pg-emph'>Looking to Game is a full-stack web app which provides a place gamers can go inorder to find a play group.</p>
+            <p className='pg-content'>Many games have matchmaking lobbies or places to join groups. A significant number of games do not however, and some that do have poorly functioning systems. This is where we come in. Sign up, creat a play group or join an existing one.</p>
+            <p className='pg-content'>Built on a week deadline, this app utilizes the following: JavaScript, NodeJS, Express, AngularJS, HTML, CSS using SCSS, RESE API stored with MongoDB, and built with test driven development.</p>
+            <div className='btn-div'>
+              <a href='#' className='btn-link'>Deployed version</a>
+            </div>
           </div>
         </div>
         <div className='project'>
           <div className='project-pic cols'>
-
+            <div className='project-title-box'>
+              <h1 className='pg-title project-title'>Decarbonate</h1>
+            </div>
           </div>
           <div className='project-info-even cols'>
-            <h1 className='pg-title'>Decarbonate</h1>
             <p className='pg-emph'></p>
             <p className='pg-content'></p>
+            <div className='btn-div'>
+              <a href='#' className='btn-link'>Github</a>
+            </div>
           </div>
         </div>
         <div className='project'>
           <div className='project-pic cols'>
-
+            <div className='project-title-box'>
+              <h1 className='pg-title project-title'>Meteorite Mapper</h1>
+            </div>
           </div>
           <div className='project-info-odd cols'>
-            <h1 className='pg-title'>Meteorite Mapper</h1>
             <p className='pg-emph'></p>
             <p className='pg-content'></p>
+            <div className='btn-div'>
+              <a href='#' className='btn-link'>Deployed version</a>
+            </div>
           </div>
         </div>
       </div>
