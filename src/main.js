6a17032eeb774d05f9e4606c7dc16e537984e506
ScottBrenden <scott.b.brenden@gmail.com>
2017-08-25 16:02:02 -0700

    start on trying to get nav dropdown working

--- src/main.js
+++ src/main.js
@@ -9,14 +9,29 @@ import {AboutContainer} from './component/about-container'
 import {ProjectsContainer} from './component/projects-container/projects'
 import {landingContainer} from './component/landing-container'
 // import {Navbar} from './component/navbar-container'
+// import {HamburgerMenu} from '../node_modules/react-hamburger-menu/dist'
 
 
 class App extends React.Component{
   constructor(props){
     super(props)
     this.state = {
+      windowWidth: window.innerWidth
+    }
+
+    handleResize() {
+      this.setState({windowWidth: window.innerWidth})
+    }
+
+    componentDidMount() {
+      window.addEventListener('resize', this.handleResize.bind(this))
+    }
+
+    componentWillUnmount() {
+      window.removeEventListener('resize', this.handleResize.bind(this))
     }
   }
+
   render(){
     let logo = '/imgs/SBtreelogoblk.png'
     return(
@@ -27,10 +42,10 @@ class App extends React.Component{
               <h1 id='name'> Scott Brenden </h1>
               <div className='nav-logo'></div>
             </div>
+            <div className='icon-align-justify menu-icon'></div>
             <div className='nav-items nav-pgs'> <a href='/'> HOME </a></div>
             <div className='nav-items nav-pgs'> <a href='/about'> ABOUT </a></div>
             <div className='nav-items nav-pgs'> <a href='/projects'> PROJECTS </a></div>
-            <div className='icon-align-justify menu-icon'></div>
           </nav>
         </header>
         <BrowserRouter>
@@ -49,3 +64,5 @@ ReactDOM.render(<App />, document.getElementById('root'))
 // const container = document.createElement('div')
 // document.body.appendChild(container)
 // ReactDOM.render(<Home />, container)
+
+// <HamburgerMenu isOpen={this.state.open} menuClicked={this.handleClick.bind(this)} className='menu-icon' />
