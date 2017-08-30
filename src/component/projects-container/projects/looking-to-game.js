afe6ec2f14a695cd6f8786d8c6e1e47e54ac027a
ScottBrenden <scott.b.brenden@gmail.com>
2017-08-30 15:51:07 -0700

    about and projects spellchecked

--- src/component/projects-container/projects/looking-to-game.js
+++ src/component/projects-container/projects/looking-to-game.js
@@ -10,8 +10,8 @@ export class LookingToGame extends React.Component {
           </div>
         </div>
         <div className='project-info-odd cols'>
-          <p className='pg-emph'>Looking to Game is a full-stack web app which provides a place gamers can go inorder to find a play group.</p>
-          <p className='pg-content'>Many games have matchmaking lobbies or places to join groups. A significant number of games do not however, and some that do have poorly functioning systems. This is where we come in. Sign up, creat a play group or join an existing one.</p>
+          <p className='pg-emph'>Looking to Game is a full-stack web app which provides a place gamers can go in-order to find a play group.</p>
+          <p className='pg-content'>Many games have matchmaking lobbies or places to join groups. A significant number of games do not however, and some that do have poorly functioning systems. This is where we come in. Sign up, create a play group or join an existing one.</p>
           <p className='pg-content'>Built on a week deadline, this app utilizes the following: JavaScript, NodeJS, Express, AngularJS, HTML, CSS using SCSS, REST API stored with MongoDB, and built with test driven development.</p>
           <div className='btn-div'>
             <a href='https://ltg-fe-production.herokuapp.com' className='btn-link'>Deployed version</a>
