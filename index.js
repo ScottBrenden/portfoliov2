3db17f8f82c67483e8139dfa2063079967fee759
ScottBrenden <scott.b.brenden@gmail.com>
2017-09-08 15:08:06 -0700

    sever file rework

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 'use strict';
 
-const fs = require('fs');
-const express = require('express');
+const express = require('express')
+const morgan = require('morgan')
 
-const app = express();
+const app = express()
 
-const PORT = process.env.PORT || 3000;
+app.use(morgan('common'))
+app.use(express.static(`${__dirname}/build`))
+app.get('*', (req, res) => res.sendFile(`${__dirname}/build/index.html`))
 
-app.use(express.static('.'));
+app.listen(process.env.PORT, () => {
+  console.log('Server runnint on port: ', process.env.PORT)
+})
 
-app.get('*', function(request, response){
-  response.sendFile('/index.html', {root: '.'});
-});
-
-app.listen(PORT, function(){
-  console.log(`on PORT localhost:${PORT}`);
-});
+// const express = require('express');
+//
+// const app = express();
+//
+// const PORT = process.env.PORT || 3000;
+//
+// app.use(express.static('.'));
+//
+// app.get('*', function(request, response){
+//   response.sendFile('/index.html', {root: '.'});
+// });
+//
+// app.listen(PORT, function(){
+//   console.log(`on PORT localhost:${PORT}`);
+// });
