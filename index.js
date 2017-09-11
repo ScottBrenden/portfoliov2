2b352eecae56e9007ac04dba7e25465c8541111a
ScottBrenden <scott.b.brenden@gmail.com>
2017-09-11 13:57:23 -0700

    added heroku post build script and engien

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 'use strict';
 
-// const express = require('express')
-// const morgan = require('morgan')
+const express = require('express')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(morgan('common'))
+app.use(express.static(`${__dirname}/build`))
+app.get('*', (req, res) => res.sendFile(`${__dirname}/build/index.html`))
+
+app.listen(process.env.PORT, () => {
+  console.log('Server runnint on port: ', process.env.PORT)
+})
+
+// const express = require('express');
 //
-// const app = express()
+// const app = express();
 //
-// app.use(morgan('common'))
-// app.use(express.static(`${__dirname}/build`))
-// app.get('*', (req, res) => res.sendFile(`${__dirname}/build/index.html`))
+// const PORT = process.env.PORT || 3000;
 //
-// app.listen(process.env.PORT, () => {
-//   console.log('Server runnint on port: ', process.env.PORT)
-// })
-
-const express = require('express');
-
-const app = express();
-
-const PORT = process.env.PORT || 3000;
-
-app.use(express.static('.'));
-
-app.get('*', function(request, response){
-  response.sendFile('/src/index.html', {root: '.'});
-});
-
-app.listen(PORT, function(){
-  console.log(`on PORT localhost:${PORT}`);
-});
+// app.use(express.static('.'));
+//
+// app.get('*', function(request, response){
+//   response.sendFile('/src/index.html', {root: '.'});
+// });
+//
+// app.listen(PORT, function(){
+//   console.log(`on PORT localhost:${PORT}`);
+// });
