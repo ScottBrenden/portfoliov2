289fe8190e32480d0d6d9585ed2eba54f17c9dfd
ScottBrenden <scott.b.brenden@gmail.com>
2017-09-20 03:56:29 -0700

    fixing landing background

--- src/component/landing-container/index.js
+++ src/component/landing-container/index.js
@@ -5,7 +5,7 @@ import './_landing.scss'
 export class landingContainer extends React.Component {
   render(){
     return(
-      <div>
+      <div id="landing-container">
         <div className='prof-pic-background'>
           <div className='prof-pic'>
           </div>
